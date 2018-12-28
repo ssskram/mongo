1bdd4181872b18bf1d04302fa94395f9a2464cbb
ssskram <paul.marks@pittsburghpa.gov>
2018-12-28 15:15:02 -0500

    first

---
+++ routes/mongo.js
@@ -0,0 +1,10 @@
+// sends email via sendgrid
+
+const express = require('express')
+const router = express.Router()
+const checkToken = require('../token')
+
+// routes here
+
+
+module.exports = router
