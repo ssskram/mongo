643e105b8a7e8cadebe2d2aa215d1c3b1ede210f
ssskram <paul.marks@pittsburghpa.gov>
2019-01-10 14:02:02 -0500

    newOrder post

--- models/pghSupply.js
+++ models/pghSupply.js
@@ -23,7 +23,40 @@ var ProfileSchema = new Schema({
   },
   collection: 'userProfiles'
 })
-
 var UserProfile = mongoose.model('UserProfile', ProfileSchema)
 
-module.exports = UserProfile
+var OrderSchema = new Schema({
+  user: String,
+  department: String,
+  location: String,
+  comments: String,
+  emergencyOrder: Boolean,
+  emergencyJustification: String,
+  narcanCases: Boolean,
+  narcanAdministeredUnknown: String,
+  miscItems: String,
+  items: [{
+    item: {
+      cartegraphID: Number,
+      itemName: String,
+      itemType: String,
+      department: String,
+    },
+    quantity: Number
+  }],
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+}, {
+  shardKey: {
+    department: null
+  },
+  collection: 'orders'
+})
+var Order = mongoose.model('Order', OrderSchema)
+
+module.exports = {
+  UserProfile,
+  Order
+}
--- routes/save.js
+++ routes/save.js
@@ -3,7 +3,10 @@
 const express = require('express')
 const router = express.Router()
 const checkToken = require('../token')
-const UserProfile = require('../models/pghSupply')
+const {
+    UserProfile,
+    Order
+} = require('../models/pghSupply')
 
 // creates and/or updates a userProfile for users on PGH Supply
 router.post('/userProfile',
@@ -21,7 +24,7 @@ router.post('/userProfile',
                             if (err) res.status(500).send(err)
                             else res.status(200).end()
                         })
-                    } else {                        
+                    } else {
                         user.department = req.body.department
                         user.cart = req.body.cart
                         user.save(err => {
@@ -49,10 +52,33 @@ router.post('/cart',
                             if (err) res.status(500).send(err)
                             else res.status(200).end()
                         })
-                    } else {     
-                        res.status(404).send()                   
+                    } else {
+                        res.status(404).send()
                     }
                 })
+        } else res.status(403).end()
+    }
+)
+
+router.post('/newOrder',
+    function (req, res) {
+        const valid = (checkToken(req.token))
+        if (valid == true) {
+            const newOrder = new Order(req.body)
+            newOrder.save(err => {
+                if (err) res.status(500).send(err)
+                else res.status(200).end()
+            })
+        } else res.status(403).end()
+    }
+)
+
+router.post('/updatedOrder',
+    function (req, res) {
+        const valid = (checkToken(req.token))
+        if (valid == true) {
+            // post to mongo here
+            res.status(202).end()
         } else res.status(403).end()
     }
 )
